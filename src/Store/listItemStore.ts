194c7043249f783a3c395f3889f9690dbf50dd9d
Thanaphorn Wutthikunprapan <t.thanaphorn.wutthikunprapan@scbtechx.io>
2022-07-11 12:41:15 +0700

    action delete

--- src/Store/listItemStore.ts
+++ src/Store/listItemStore.ts
@@ -12,21 +12,26 @@ export const listItemStore = createSlice({
   initialState: initialState,
   reducers: {
     add: (state, action: PayloadAction<{ item: string }>) => {
+      console.log(state.items);
       return {
         ...state,
         items: [...state.items, action.payload.item],
       };
     },
-    // delete: (state) => {
-    //   return {
-    //     ...state,
-    //     items: [...state.items, initialState.items],
-    //   };
-    // },
+    del: (state, action: PayloadAction<{ index: number }>) => {
+      return {
+        ...state,
+        items: state.items.filter((item, index) => {
+          return index !== action.payload.index;
+        }),
+      };
+    },
     // edit: (state) => {
     //   return { ...state, item: (state.item = 0) };
     // },
   },
 });
 
-export const { add } = listItemStore.actions;
+export const { add, del } = listItemStore.actions;
+// export const add = listItemStore.actions.add;
+// export const delete = listItemStore.actions.delete;
