8c35a174cb1c8eca392fa68aa40bc2ee34eb366d
Thanaphorn Wutthikunprapan <t.thanaphorn.wutthikunprapan@scbtechx.io>
2022-07-11 12:41:42 +0700

    delete

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { del } from "../../Store/listItemStore";
 import { rootType } from "../../Store/rootReducer";
+import { store } from "../../Store/store";
 import style from "./Landing.module.css";
 
 function Landing() {
@@ -11,13 +13,20 @@ function Landing() {
   return (
     <div className={style.container}>
       <h1>My Todo List</h1>
-      {state.items.map((item) => {
+      {state.items.map((item, index) => {
         return (
           <div className={style.showItem}>
             <div className={style.item}>{item}</div>
             <div className={style.actionBtn}>
               <div className={style.edit}>edit</div>
-              <div className={style.delete}>delete</div>
+              <div
+                className={style.delete}
+                onClick={() => {
+                  store.dispatch(del({ index: index }));
+                }}
+              >
+                delete
+              </div>
             </div>
           </div>
         );
